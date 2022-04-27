240a8da577da116bb48ed804c7707b2f72f584c2
jainayushee <jainayushee5@gmail.com>
2022-04-27 12:55:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anew/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.anew;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    TextToSpeech t1;
+    EditText ed1;
+    Button b1, b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed1=(EditText)findViewById(R.id.text);
+        b1=(Button)findViewById(R.id.button);
+        b2= findViewById(R.id.next);
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = ed1.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent= new Intent(MainActivity.this, MainActivity2.class);
+//                startActivity(intent);jo4ojo4hto5otoj5hyo43h6
+            }
+        });
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+}
